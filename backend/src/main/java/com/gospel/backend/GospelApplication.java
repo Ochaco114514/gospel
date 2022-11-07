a7caba90c1d61c305421d7bb7923a0045a4c34b8
Max <86398378+titiac@users.noreply.github.com>
2022-11-07 21:29:19 +0800

    初始化项目

---
+++ backend/src/main/java/com/gospel/backend/GospelApplication.java
@@ -0,0 +1,13 @@
+package com.gospel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GospelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GospelApplication.class, args);
+    }
+
+}
